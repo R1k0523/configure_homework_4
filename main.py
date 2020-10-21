12a49132ed39922bad64c8c1aee2042a6decf070
R1k0523 <44803650+R1k0523@users.noreply.github.com>
2020-10-21 22:22:41 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+import json
+from pathlib import Path
+from sly import Lexer, Parser
+"""
+directives ::= directive directives | empty
+directive ::= simple_directive | block_directive
+names ::= name names | empty
+simple_directive ::= name names ";"
+simple_directive ::= name names "{" directives "}"
+"""
+
+class ConfLexer(Lexer):
+    tokens = {BEGIN, END, NAME, SEMICOL, STRING}
+    BEGIN = r'{'
+    END = r'}'
+    ignore = r' \t'
+    ignore_comment = r'\#.*'
+    ignore_newline = r'\n+'
+    NAME = r'[^ \t\#{};\']+'
+    SEMICOL = r';'
+    STRING = r"'.*'"
+
+class ConfParser(Parser):
+    tokens = ConfLexer.tokens
+
+    @_('directive directives')
+    def directives(self, p):
+        return [p.directive] + p.directives
+
+    @_('empty')
+    def directives(self, p):
+        return []
+
+    @_('simple_directive', 'block_directives')
+    def directive(self, p):
+        return p[0]
+
+    @_('name names SEMICOL')
+    def simple_directive(self, p):
+        return dict(type="simple", args=[p.name] + p.names, ctx=[])
+
+    @_('name names BEGIN directives END')
+    def block_directives(self, p):
+        return dict(type="block", args=[p.name] + p.names, ctx=p.directives)
+
+    @_('name names')
+    def names(self, p):
+        return [p.name] + p.names
+
+    @_('empty')
+    def names(self, p):
+        return []
+
+    @_('NAME')
+    def name(self, p):
+        return p[0]
+
+    @_('STRING')
+    def name(self, p):
+        return p[0][1:-1]
+
+
+    @_('')
+    def empty(self, p):
+        pass
+
+
+text = Path("nginx.conf").read_text()
+
+lexer = ConfLexer()
+parser = ConfParser()
+data = parser.parse(lexer.tokenize(text))
+Path("result.json").write_text(json.dumps(data, indent=2))
